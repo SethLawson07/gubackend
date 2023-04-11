5802b5ae5fe66caa95e2c1c0d29cb40469765677
TheWisePigeon <zozozozeph@gmail.com>
2023-04-11 15:39:21 +0000

    feat: update item

--- src/services/Item.ts
+++ src/services/Item.ts
@@ -61,3 +61,58 @@ export async function get(_req: Request, res: Response){
         return res.status(500).send()
     }
 }
+
+export async function update(req: Request, res:Response ){
+    try {
+       const schema = z.object({
+           id: z.string({ required_error:"id est un parametre requis", invalid_type_error:"id doit etre un string"}),
+           name: z.string({ invalid_type_error:"name doit etre un string"}).optional(),
+           featured: z.boolean({ invalid_type_error:"featured doit etre un string"}).optional(),
+       }) 
+       const validation_result = schema.safeParse(req.body)
+       if(!validation_result.success) return res.status(400).send({ message: JSON.parse(validation_result.error.message)})
+       const { data } = validation_result
+       const targetted_item = await prisma.item.findUnique({
+           where:{
+               id: data.id
+           }
+       })
+       if(!targetted_item) return res.status(404).send()
+        const potential_duplicate = await prisma.item.findFirst({
+            where:{
+                name: data.name,
+                sub_category: targetted_item.sub_category
+            }
+        })
+        if(potential_duplicate) return res.status(409).send()
+        await prisma.item.update({
+            where:{
+                id:data.id
+            },
+            data
+        })
+        return res.status(200).send()
+    } catch (err) {
+        console.error(`Error while updating item's info`)
+        return res.status(500).send()
+    }
+}
+
+export async function delete_(req: Request, res: Response){
+    try {
+        const schema = z.object({
+            id: z.string({ required_error:"id est un parametre requis", invalid_type_error:"id doit etre un string"})
+        })
+        const validation_result = schema.safeParse(req.body)
+        if(!validation_result.success) return res.status(400).send({ message:"id manquand ou de type incorrect"})
+        const { id } = validation_result.data
+        return targetted_item = await prisma.item.findUnique({
+            id
+        })
+        if(!targetted_item) return res.status(404).send()
+        return res.status(200).send()
+    } catch (err) {
+        console.error(`Error while deleting item ${err}`)
+        return res.status(500).send()
+    }
+}
