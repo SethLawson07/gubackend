2af164774170a31fb51ab72f8c3e9a3d581aa7fb
TheWisePigeon <zozozozeph@gmail.com>
2023-04-11 11:34:09 +0000

    feat: update subcategory

--- src/services/SubCategory.ts
+++ src/services/SubCategory.ts
@@ -44,3 +44,42 @@ export async function get(_req: Request, res: Response){
         return res.status(500).send()
     }
 }
+
+export async function update(req: Request, res: Response){
+    try {
+        const schema = z.object({
+            id: z.string({ required_error:"id est un parametre requis", invalid_type_error:"id doit etre un string"}),
+            name: z.string({ invalid_type_error:"name doit etre un string" }).optional(),
+            featured: z.boolean({ invalid_type_error:"featured doit etre un booleen"}).optional()
+        })
+        const validation_result = schema.safeParse(req.body)
+        if(!validation_result.success) return res.status(400).send({ message: JSON.parse(validation_result.error.message)})
+        const { data } = validation_result
+        const targetted_subcategory = await prisma.subCategory.findUnique({
+            where:{
+                id:data.id
+            }
+        })
+        if(!targetted_subcategory) return res.status(404).send()
+        const potential_duplicate = await prisma.subCategory.findFirst({
+            where:{
+                category:targetted_subcategory.category,
+                name:data.name
+            }
+        })
+        if(potential_duplicate) return res.status(409).send()
+        await prisma.subCategory.update({
+            where:{
+                id: data.id
+            },
+            data:{
+                name:data.name,
+                featured:data.featured
+            }
+        })
+        return res.status(200).send()
+    } catch (err) {
+        console.error(`Error while updating subcategory ${err}`)
+        return res.status(500).send()
+    }
+}
