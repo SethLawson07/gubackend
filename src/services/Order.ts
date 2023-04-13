cad3553d930fe41a66291d258baf0b75bde5d113
TheWisePigeon <zozozozeph@gmail.com>
2023-04-13 11:30:36 +0000

    feat: cancel  order

--- src/services/Order.ts
+++ src/services/Order.ts
@@ -2,8 +2,6 @@ import { Request, Response } from "express";
 import { z } from "zod";
 import { prisma } from "../server";
 
-type OrderStatus = "PENDING" | "CANCELLED"
-
 export async function create(req: Request, res: Response){
     try {
         const schema = z.object({
@@ -72,7 +70,29 @@ export async function cancel_order(req: Request, res: Response){
         const validation_result = schema.safeParse(req.body)
         if(!validation_result.success) return res.status(400).send()
         const { id } = validation_result.data
-        
+        const { user } = req.body.user as { user: string} 
+        const current_user = await prisma.user.findUnique({
+            where:{
+                email:user
+            }
+        })
+        if(!current_user) return res.status(401).send()
+        const targetted_order = await prisma.order.findUnique({
+            where:{
+                id
+            }
+        })
+        if(!targetted_order) return res.status(404).send()
+        if(targetted_order.user!==current_user.id) return res.status(403).send()
+        await prisma.order.update({
+            where:{
+                id
+            },
+            data:{
+                status:"CANCELLED"
+            }
+        })
+        return res.status(200).send()
     } catch (err) {
         console.error(`Error while cancelling order ${err}`)
         return res.status(500).send()
