0a609775d6a79d2d9c3a9792edd45e27e482ac62
TheWisePigeon <zozozozeph@gmail.com>
2023-04-13 09:15:36 +0000

    feat: add promo code conditions logic

--- src/services/PromoCode.ts
+++ src/services/PromoCode.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 import { Request, Response } from "express";
 import { prisma } from "../server";
 
-export async function create(req: Request, res: Response){
+export async function create(req: Request, res: Response) {
     try {
         const schema = z.object({
             code: z.string(),
@@ -13,17 +13,17 @@ export async function create(req: Request, res: Response){
             expiration_date: z.string()
         })
         const validation_result = schema.safeParse(req.body)
-        if(!validation_result.success) return res.status(400).send({ message: JSON.parse(validation_result.error.message)})
+        if (!validation_result.success) return res.status(400).send({ message: JSON.parse(validation_result.error.message) })
         const { data } = validation_result
-        if(isNaN(Date.parse(data.expiration_date))) return res.status(400).send({ message:"date invalide"})
-        
+        if (isNaN(Date.parse(data.expiration_date))) return res.status(400).send({ message: "date invalide" })
+
     } catch (err) {
         console.error(`Error while creating promocode ${err}`)
         return res.status(500).send()
     }
 }
 
-export async function get(_req: Request, res: Response){
+export async function get(_req: Request, res: Response) {
     try {
         const data = await prisma.promoCode.findMany()
         return res.status(200).send({ data })
@@ -33,7 +33,7 @@ export async function get(_req: Request, res: Response){
     }
 }
 
-export async function verify(req: Request, res: Response){
+export async function verify(req: Request, res: Response) {
     try {
         const schema = z.object({
             amount: z.number().positive(),
@@ -43,16 +43,19 @@ export async function verify(req: Request, res: Response){
             })),
             code: z.string()
         })
-        const { user }  = req.body.user as { user: string }
+        const { user } = req.body.user as { user: string }
         const current_user = await prisma.user.findUnique({
-            where:{
+            where: {
                 email: user
             }
         })
-        if(!current_user) return res.status(401).send()
+        if (!current_user) return res.status(401).send()
         const validation_result = schema.safeParse(req.body)
-        if(!validation_result.success) return res.status(400).send()
+        if (!validation_result.success) return res.status(400).send()
         const { data } = validation_result
+        //check if user did not already use promo code
+        //check if order is elligible for promo code or if promo code is still valid
+        return res.status(200).send({ new_amount: 100 })
     } catch (err) {
         console.error(`Error while verifying promo code ${err}`)
         return res.status(500).send()
--- src/utils/conditions.ts
+++ src/utils/conditions.ts
@@ -1,11 +1,13 @@
 //import { Request, Response } from "express"
 //import { prisma } from "../server"
 import { Product } from "@prisma/client"
+import { prisma } from "../server"
+import { deflate } from "zlib"
 
 type Condition = {
-    id: number,
+    id: string,
     description: string
-    condition: ( order:OrderData )=>boolean
+    condition: ( order:OrderData, param:string|undefined )=>Promise<boolean>
 }
 
 type OrderData = {
@@ -15,3 +17,44 @@ type OrderData = {
     products: Array<Product>
 }
 
+const conditions: Condition[] = [
+    {
+        id:"0",
+        description:"Applicable seulement pour les nouveaux utilisateurs",
+        condition:async (order: OrderData, _param: string|undefined)=>{
+            const current_user = await prisma.user.findUnique({
+                where:{
+                    email: order.user
+                }
+            })
+            if(!current_user) return false
+            const user_orders = await prisma.order.findMany({
+                where:{
+                    user: current_user.id
+                }
+            })
+            if(user_orders.length!==0) return false
+            return true
+        }
+    },
+    {
+        id:"1",
+        description:"Applicable seulement pour les commandes au dela d'un montant donne",
+        condition: async (order: OrderData, param: string|undefined)=>{
+            if(!param) return false
+            const min_amount = Number(param as string)
+            if(isNaN(min_amount)) return false
+            return min_amount>order.amount
+        }
+    }
+]
+
+export async function apply_conditions(given_conditions:string[], order: OrderData){
+    for (const condition in given_conditions) {
+        const targetted_condition = conditions.find((c)=>c.id==condition.split(":")[0])
+        if(!targetted_condition) continue
+        const condition_is_valid = await targetted_condition.condition(order, condition.split(":")[1])
+        if(!condition_is_valid) return false
+    }
+    return true
+}
