fca8c392dadad432df0d3b6b71d39964be2c3326
TheWisePigeon <zozozozeph@gmail.com>
2023-04-13 09:47:11 +0000

    docs: add promo code swagger doc

---
+++ src/routes/promocode/index.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { create, get, delete_, verify} from "../../services/PromoCode"
+import { Auth, UserIsAdmin } from "../../utils/middlewares";
+
+const router = Router()
+
+router.route("").post( Auth, UserIsAdmin, create )
+router.route("").get( Auth, UserIsAdmin, get )
+router.route("/verify").post( Auth, verify )
+router.route("").delete( Auth, UserIsAdmin, delete_ )
+
+export default router
--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,7 @@ import subcategory from "./routes/subcategory"
 import item from "./routes/item"
 import product from "./routes/product"
 import payment from "./routes/payment"
+import promocode from "./routes/promocode"
 
 const PORT = process.env.PORT || 5000
 
@@ -33,6 +34,7 @@ app.use("/subcategory", subcategory)
 app.use("/item", item)
 app.use("/product", product)
 app.use("/pay", payment)
+app.use("/promocode", promocode)
 
 app.get("/health", (_req: Request, res: Response) => {
     return res.status(200).send()
