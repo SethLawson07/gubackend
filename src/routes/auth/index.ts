2b1b7f1d5ff736f367db8e6d6f8051fdf967df25
TheWisePigeon <zozozozeph@gmail.com>
2023-04-19 15:43:35 +0000

    feat: add new auth routes

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { register, login, create_admin, get_orders} from "../../services/Auth"
+import { register, login, create_admin, get_orders, change_password, set_financepro_id } from "../../services/Auth"
 import { Auth, UserIsAdmin } from "../../utils/middlewares"
 
 const router = Router()
@@ -11,5 +11,9 @@ router.route("/login").post( login )
 router.route("/create_admin").post( Auth, UserIsAdmin, create_admin  )
 
 router.route("/orders").get( Auth, get_orders )
+
+router.route("/change_password").put( Auth, change_password)
+
+router.route("/set_financepro_id").post( Auth, UserIsAdmin, set_financepro_id )
 
 export default router
