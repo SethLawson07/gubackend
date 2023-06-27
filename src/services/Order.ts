cb77710f07d2d8dc58e144d1bcf497bc059996e3
Lonie <lonierachid@gmail.com>
2023-06-27 22:03:17 +0000

    ...

--- src/services/Order.ts
+++ src/services/Order.ts
@@ -49,7 +49,7 @@ export async function create(req: Request, res: Response) {
         })
         const response = await generate_payment_link(data.amount, current_user.id, order.id)
         console.log(response)
-        return res.status(200).send({ data: response, message: "Order created" })
+        return res.status(200).send({ data: response, order: order.id })
     } catch (err) {
         console.log(`Error while creating order ${err}`)
         return res.status(500).send()
