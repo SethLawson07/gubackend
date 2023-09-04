1f957d983a5e47d73ea13b3045d46baf577e200c
Lonie <lonierachid@gmail.com>
2023-09-04 21:28:30 +0000

    ...

--- src/routes/service/index.ts
+++ src/routes/service/index.ts
@@ -1,11 +1,18 @@
 import { Router } from "express"
-import { create, get } from "../../services/Service"
+import { create, create_service_category, get, service_categories } from "../../services/Service"
 import { Auth, UserIsAdmin } from "../../utils/middlewares"
 
 const router = Router();
+
+// Add Service
 router.route("").post(Auth, UserIsAdmin, create);
+// Services
 router.route("").get(get);
 
+// Category Services
+router.route("/category").get(service_categories);
+// Create category service
+router.route("/category").post(Auth, UserIsAdmin, create_service_category);
 
 
 export default router;
--- src/services/Service.ts
+++ src/services/Service.ts
@@ -9,13 +9,14 @@ export async function create(req: Request, res: Response) {
     const schema = z.object({
       name: z.string().min(5, "Donnez un nom de service"),
       description: z.string().min(10, "Donnez une description de service"),
+      category: z.string().nonempty("Sélectionner une catégorie"),
       pictures: z.array(z.string()).nonempty("Sélectionner des images"),
       seller_number: z.string().min(8, "Numéro de téléphone invalide").max(8, "Numéro de téléphone invalide").startsWith('9' || '7', "Numéro de téléphone invalide").nonempty("Veuillez renseigner un numéro de téléphone"),
     })
     const validation_result = schema.safeParse(req.body);
     if (!validation_result.success) return res.status(400).send({ status: 400, error: true, message: fromZodError(validation_result.error).details[0].message, data: {} })
     const { data } = validation_result
-    await prisma.services.create({
+    await prisma.service.create({
       data
     })
     return res.status(201).send({ status: 201, error: false, message: 'sucess', data: {} })
@@ -28,7 +29,7 @@ export async function create(req: Request, res: Response) {
 
 export async function get(_req: Request, res: Response) {
   try {
-    const data = await prisma.services.findMany()
+    const data = await prisma.service.findMany()
     return res.status(200).send({ error: false, message: "", data: data })
   } catch (err) {
     console.error(`Error while getting services ${err}`)
@@ -36,3 +37,32 @@ export async function get(_req: Request, res: Response) {
   }
 }
 
+
+export async function create_service_category(req: Request, res: Response) {
+  try {
+    const schema = z.object({
+      name: z.string().nonempty("Veuillez indiquez un nom pour la catégories"),
+      image: z.string(),
+      featured: z.boolean(),
+    });
+    const validation_result = schema.safeParse(req.body);
+    if (!validation_result.success) return res.status(400).send({ error: true, message: fromZodError(validation_result.error).details[0].message, data: {} });
+    const { data } = validation_result;
+    let result = await prisma.serviceCategory.create({ data });
+    if (result) return res.status(201).send({ error: false, message: "Requête aboutie", data });
+    return res.status(401).send({ error: true, message: "Erreur inconnue", data: {} });
+  } catch (err) {
+    console.error(`Error while getting services ${err}`);
+    return res.status(500).send({ error: true, message: "Une erreur est survenue", data: {} })
+  }
+}
+
+
+export async function service_categories(req: Request, res: Response) {
+  try {
+    const data = await prisma.serviceCategory.findMany();
+    return res.status(200).send({ error: true, message: "R.A", data })
+  } catch (error) {
+    console.error(`${error}`);
+  }
+}
