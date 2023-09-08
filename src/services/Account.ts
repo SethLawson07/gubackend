e53fb22a92688105d86efcfb36ae8ca97df2f5d7
Lonie <lonierachid@gmail.com>
2023-09-08 15:46:00 +0000

    ...

--- src/services/Account.ts
+++ src/services/Account.ts
@@ -299,7 +299,7 @@ export async function validate_contribution(req: Request, res: Response) {
                 if (validated) {
                     const targeted_acount = await prisma.account.findFirst({ where: { user: customer?.id! } });
                     var amount = (targeted_acount?.amount! + targeted_contribution.amount)
-                    await prisma.account.update({ where: { id: targeted_acount?.id! }, data: { amount: amount } });
+                    if (user.role == "admin") await prisma.account.update({ where: { id: targeted_acount?.id! }, data: { amount: amount } });
                     await prisma.book.update({ where: { id: book?.id! }, data: { sheets: result.updated_sheets! } });
                     if (user.role == "admin" && customer?.device_token!) await sendPushNotification(customer?.device_token!, "Cotisation", `Votre cotisation en attente vient d'être validé`);
                     return res.status(200).send({ status: 200, error: false, message: "Cotisation validée", data: validated! });
