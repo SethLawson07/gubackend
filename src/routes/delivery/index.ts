e74b90913c19e438fc70e0b60f9a3a921091f07e
Lonie <lonierachid@gmail.com>
2023-10-29 08:52:54 +0000

    ...

--- src/routes/delivery/index.ts
+++ src/routes/delivery/index.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { Auth, UserIsAdmin, UserIsDeliveryMan } from "../../utils/middlewares";
-import { all_delivery, awaiting_deliveries, delivered, gain_delivery, start_delivery } from "../../services/Delivery";
+import { all_delivery, awaiting_deliveries, delivered, delivery_by_user, gain_delivery, start_delivery } from "../../services/Delivery";
 
 const router = Router()
 
 router.route("/all").get(Auth, all_delivery);
 router.route("/awaiting").get(Auth, awaiting_deliveries);
+router.route("/user/delivery").get(Auth, delivery_by_user);
 router.route("/gain").post(Auth, gain_delivery);
 router.route("/start").post(Auth, start_delivery);
 router.route("/deliver").post(Auth, delivered);
--- src/services/Delivery.ts
+++ src/services/Delivery.ts
@@ -51,6 +51,18 @@ export async function awaiting_deliveries(req: Request, res: Response) {
     }
 }
 
+// Delivries for all deliverypersoons
+export async function delivery_by_user(req: Request, res: Response) {
+    try {
+        const { user } = req.body.user as { user: User };
+        const data = await prisma.delivery.findMany({ where: { status: "GAINED", userId: user.id } });
+        return res.status(200).send({ error: false, data, message: "ok" });
+    } catch (err) {
+        console.error(`Error while cancelling order ${err}`)
+        return res.status(500).send({ error: true, message: "Une erreur s'es produite", data: {} })
+    }
+}
+
 // Gain delivery
 // Choisir de livrer la commande
 export async function gain_delivery(req: Request, res: Response) {
@@ -81,7 +93,6 @@ export async function start_delivery(req: Request, res: Response) {
         const validation_result = schema.safeParse(req.body);
         if (!validation_result.success) return res.status(400).send({ error: true, message: "Id requis", data: {} });
         const { id } = validation_result.data;
-        const { user } = req.body.user as { user: User };
         const targetted_delivery = await prisma.delivery.findUnique({ where: { id } });
         if (!targetted_delivery) return res.status(404).send({ error: true, message: "Order not found", data: {} });
         await prisma.delivery.update({ where: { id }, data: { status: "PENDING" } });
