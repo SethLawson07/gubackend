5c42b9ee9fc0ee723725691869d1b7782ec72c96
Lonie <lonierachid@gmail.com>
2023-11-28 05:08:39 +0000

    ...

--- src/services/Home.ts
+++ src/services/Home.ts
@@ -4,10 +4,12 @@ import { products_byids } from "../utils";
 
 export async function clientHome1(_req: Request, res: Response) {
     try {
-        const categories = await prisma.category.findMany({});
-        const subcategories = await prisma.subCategory.findMany({});
-        const items = await prisma.item.findMany({});
-        const brands = await prisma.brand.findMany({});
+        const categories = await prisma.category.findMany();
+        const subcategories = await prisma.subCategory.findMany({
+            include: { category_data: { select: { name: true, id: true } } }
+        });
+        const items = await prisma.item.findMany();
+        const brands = await prisma.brand.findMany();
         return res.status(200).send({ data: { categories, subcategories, items, brands } });
     } catch (err) {
         console.error(`Error while getting list of all items ${err}`)
@@ -17,17 +19,10 @@ export async function clientHome1(_req: Request, res: Response) {
 
 export async function clientHome2(_req: Request, res: Response) {
     try {
-        const products = await prisma.product.findMany({});
-        const services = await prisma.service.findMany({});
-        const cservices = await prisma.serviceCategory.findMany({});
-        // let sections: Object[] = [];
+        const products = await prisma.product.findMany();
+        const services = await prisma.service.findMany();
+        const cservices = await prisma.serviceCategory.findMany();
         const sections = await prisma.section.findMany({ include: { content: true } });
-        // if (!target_all) return res.status(401).send({ error: true, message: "Une erreur est survenue", data: {} });
-        // for (let i = 0; i < target_all.length; i++) {
-        //     const element = target_all[i];
-        //     const products = (await products_byids(element.content)).data;
-        //     sections.push({ ...element, products });
-        // }
         return res.status(200).send({ data: { products, sections, services, cservices } });
     } catch (err) {
         console.error(`Error while getting list of all items ${err}`)
