a75a30ab021c89149b67ca2bd68116c372fcc130
Lonie <lonierachid@gmail.com>
2023-12-31 08:09:02 +0000

    ...

--- src/queues/worker.ts
+++ src/queues/worker.ts
@@ -21,22 +21,20 @@ const validateContributionWorkerHandler = async (job: Job) => {
             const agent_acount = await prisma.account.findFirst({ where: { user: customer?.agentId! } });
             const agent_benefit = (result.sheet.bet! * 20) / 100;
             const goodness_benefit = result.sheet.bet! - agent_benefit;
-            await prisma.account.update({ where: { id: user_acount?.id! }, data: { amount: (amount - result.sheet.bet!) } });
-            await prisma.account.update({ where: { id: agent_acount?.id! }, data: { amount: agent_acount?.amount! + agent_benefit } });
-            await prisma.betForReport.create({
-                data: {
-                    gooAmount: goodness_benefit,
-                    AgeAmount: agent_benefit,
-                    createdat: todateTime(schemadata.validatedat),
-                    agentId: customer?.agentId!,
-                    customerId: customer?.id!,
-                }
-            })
+            const [uaUpdate, aaUpdate] = await prisma.$transaction([
+                prisma.account.update({ where: { id: user_acount?.id! }, data: { amount: (amount - result.sheet.bet!) } }),
+                prisma.account.update({ where: { id: agent_acount?.id! }, data: { amount: agent_acount?.amount! + agent_benefit } }),
+            ]);
+            if (uaUpdate && aaUpdate) {
+                await prisma.betForReport.create({
+                    data: { gooAmount: goodness_benefit, AgeAmount: agent_benefit, createdat: todateTime(schemadata.validatedat), agentId: customer?.agentId!, customerId: customer?.id!, }
+                });
+            }
         }
         else { await prisma.account.update({ where: { id: user_acount?.id! }, data: { amount: amount } }); }
     }
-    await prisma.report.update({ where: { id: validated.reportId }, data: { status: validated.status, } });
     await prisma.book.update({ where: { id: book.data.id }, data: { sheets: result.updated_sheets! } });
+    await prisma.report.update({ where: { id: validated.reportId }, data: { status: validated.status, } });
     if (user.role == "admin" && customer?.device_token!) { await sendPushNotification(customer?.device_token!, "Cotisation", `Votre cotisation en attente vient d'être validé`); };
 }
 
@@ -47,11 +45,8 @@ const mMoneyContributionWorkerHandler = async (job: Job) => {
 
     const targeted_acount = await prisma.account.findFirst({ where: { user: data.customer } });
     let amount = (targeted_acount?.amount! + data.amount);
-    if (result.cases!.includes(1)) {
-        await prisma.account.update({ where: { id: targeted_acount?.id! }, data: { amount: (amount - result.sheet?.bet!) } });
-    } else {
-        await prisma.account.update({ where: { id: targeted_acount?.id! }, data: { amount: amount } });
-    }
+    if (result.cases!.includes(1)) { await prisma.account.update({ where: { id: targeted_acount?.id! }, data: { amount: (amount - result.sheet?.bet!) } }); }
+    else { await prisma.account.update({ where: { id: targeted_acount?.id! }, data: { amount: amount } }); }
     await prisma.book.update({ where: { id: book.data.id }, data: { sheets: result.updated_sheets! } });
     await prisma.report.update({ where: { id: report.id }, data: { status: "paid" } });
 }
